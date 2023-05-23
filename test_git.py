b5cb0720f8cd8d5acc4a94b0dcb0795feff222a6
MrFoxe <jereaperez3@gmail.com>
2023-05-23 11:03:12 -0300

    test_git.py

--- test_git.py
+++ test_git.py
@@ -1,7 +1,10 @@
 #hola mundogu
 """
-git push para subir cambios al repositorio
+git push para subir cambios al repositorio online
 
-git pull para bajar los cambios del repositorio
+git pull para bajar los cambios del repositorio 
+
+git commit -m "test_git.py" subir al repositorio local
+
 
 """
