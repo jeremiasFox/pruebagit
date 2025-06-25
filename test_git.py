d9dfb857afa2414acfb8653478e240588eca9c58
MrFox <53784007+jeremiasFox@users.noreply.github.com>
2025-06-25 19:21:30 -0300

    Update test_git.py

--- test_git.py
+++ test_git.py
@@ -8,6 +8,13 @@ git push para subir cambios al repositorio online
 
 git pull para bajar los cambios del repositorio 
 
+git status (ver el estado actual)
+git diff (ver que cambió dentro de cada archivo)
+git add <nombre del archivo> (instancia el archivo para hacer un commit)
+git commit -m "<mensaje>" (hacen el commit)
+git push (suben su commit al repo)
+git pull (traen los commits del resto del repo a su compu)
+
 
 
 """
